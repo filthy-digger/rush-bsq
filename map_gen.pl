0ddbd98274a105f2a70e3633bb2f289c510f2725
argabaso <argabaso@student.42lausanne.ch>
2025-07-14 23:01:55 +0200

    refactor map generation to support optional pattern input

--- map_gen.pl
+++ map_gen.pl
@@ -2,16 +2,25 @@
 
 use warnings;
 use strict;
-die "program x y density" unless (scalar(@ARGV) == 3);
-my ($x, $y, $density) = @ARGV;
-print "$y.ox\n";
+my ($x, $y, $density, $pattern);
+die "program x y density (optional pattern)\n" unless (scalar(@ARGV) == 3 || scalar(@ARGV) == 4);
+
+if (scalar(@ARGV) == 4) {
+	($x, $y, $density, $pattern) = @ARGV;
+	die "pattern must me 3 characters long" unless (length($pattern) == 3);
+}
+else {
+	($x, $y, $density) = @ARGV;
+	$pattern = ".ox";
+}
+print "$y$pattern\n";
 for (my $i = 0; $i < $y; $i++) {
 	for (my $j = 0; $j < $x; $j++) {
 		if (int(rand($y) * 2) < $density) {
-			print "o";
+			print substr($pattern, 1, 1);  # Second character (obstacle)
 		}
 		else {
-			print ".";
+			print substr($pattern, 0, 1);  # First character (empty)
 		}
 	}
 	print "\n";
